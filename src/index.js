62d529ca02f06e99a2201f67cfc8d98bdde3513c
Saba Ahmad <sabaahmad895@gmail.com>
2022-12-07 23:57:12 +0500

    some linter stuff

--- src/index.js
+++ src/index.js
@@ -1,37 +1,38 @@
-import "./style.css";
-import dots from "./images/dots.png";
-let arr = [
+import './style.css';
+import dots from './images/dots.png';
+
+const arr = [
   {
-    description: "wash the dishes",
+    description: 'wash the dishes',
     completed: true,
     index: 1,
   },
   {
-    description: "clean the house",
+    description: 'clean the house',
     completed: false,
     index: 2,
   },
   {
-    description: "complete the project",
+    description: 'complete the project',
     completed: true,
     index: 3,
   },
 ];
 
-const list = document.querySelector(".container");
+const list = document.querySelector('.container');
 for (let i = 0; i < arr.length; i += 1) {
-  const itemOfList = document.createElement("div");
-  itemOfList.classList.add("list-class");
+  const itemOfList = document.createElement('div');
+  itemOfList.classList.add('list-class');
   list.append(itemOfList);
-  const box = document.createElement("input");
-  box.type = "checkbox";
+  const box = document.createElement('input');
+  box.type = 'checkbox';
   itemOfList.append(box);
-  const paragraph = document.createElement("p");
-  paragraph.classList.add("para-text");
+  const paragraph = document.createElement('p');
+  paragraph.classList.add('para-text');
   paragraph.innerText = arr[i].description;
   itemOfList.append(paragraph);
-  const imgs = document.createElement("img");
-  imgs.classList.add("list-images");
+  const imgs = document.createElement('img');
+  imgs.classList.add('list-images');
   itemOfList.append(imgs);
   imgs.src = dots;
 }
