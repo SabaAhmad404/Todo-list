804012381c047c176ee77b0f5afca5d1445a5818
Saba Ahmad <sabaahmad895@gmail.com>
2022-12-07 14:25:51 +0500

    mainb-branch-files and folders

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Todo-list
+# webpack-exercise
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    index: "./src/index.js",
+  },
+  devServer: {
+    static: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  output: {
+    filename: "[name].main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  optimization: {
+    runtimeChunk: "single",
+  },
+};
